ec2592ef7e13761da8690a941082cce9f60d58e7
Claudio Suarez <csuarez689@gmail.com>
2025-11-05 15:31:38 -0300

    logout

--- app/src/main/java/com/csuarez/ulpinmobiliaria/ui/menu/MenuActivity.java
+++ app/src/main/java/com/csuarez/ulpinmobiliaria/ui/menu/MenuActivity.java
@@ -1,12 +1,21 @@
 package com.csuarez.ulpinmobiliaria.ui.menu;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import com.csuarez.ulpinmobiliaria.MainActivity;
 import com.csuarez.ulpinmobiliaria.R;
 import com.csuarez.ulpinmobiliaria.databinding.ActivityMenuBinding;
+import com.csuarez.ulpinmobiliaria.network.ApiClient;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,10 +24,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMenuBinding binding;
+
+    private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +41,38 @@ public class MenuActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.appBarMenu.toolbar);
 
+        drawer = binding.drawerLayout;
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
 
 
         appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home)
+                R.id.nav_home,R.id.nav_perfil)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu);
+
+        if (id == R.id.nav_logout) {
+            mostrarDialogLogout();
+        } else {
+            NavigationUI.onNavDestinationSelected(item, navController);
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -53,5 +86,26 @@ public class MenuActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void mostrarDialogLogout() {
+        new AlertDialog.Builder(this)
+                .setTitle("Cerrar sesión")
+                .setMessage("¿Esta seguro que desea cerrar la sesión actual?")
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        ApiClient.saveToken(getApplicationContext(),"");
+                        Snackbar.make(binding.getRoot(), "Sesión cerrada correctamente", Snackbar.LENGTH_SHORT).show();
+
+
+                        Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Cancelar", null)
+                .show();
     }
 }
--- app/src/main/java/com/csuarez/ulpinmobiliaria/ui/menu/logout/LogoutViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.csuarez.ulpinmobiliaria.ui.menu.logout;
-
-import androidx.lifecycle.ViewModel;
-
-public class LogoutViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
